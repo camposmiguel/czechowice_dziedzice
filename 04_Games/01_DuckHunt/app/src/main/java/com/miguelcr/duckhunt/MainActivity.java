c6fac0cfe89360c903261c0af602ebba9dd9b6cd
Miguel Campos <camposmiguel@gmail.com>
2017-03-13 16:09:22 +0100

    resources

---
+++ 04_Games/01_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
@@ -0,0 +1,17 @@
+package com.miguelcr.duckhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
